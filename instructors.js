a87e7a34cd4ffbeb2d78399d4ad37cd0cc3b6cde
ederdaniel22 <60990935+ederdaniel22@users.noreply.github.com>
2020-09-01 18:56:41 -0400

    Page instructors, formulários.
    Criação final da página de cadastro Instructors, criação da página data.json, botão de edição e styles.

--- instructors.js
+++ instructors.js
@@ -1,35 +1,54 @@
-const fs = require('fs')
-const data = require('./data.json')
+    const fs = require('fs')
+    const data = require('./data.json')
+    const { age } = require('./utils')
 
-exports.post = function(req, res) {
+    exports.show = function(req, res) {
+        //req.params.id = /
+        const { id } = req.params
+        const foundInstructor = data.instructors.find(function(instructor) {
+            return instructor.id == id
+        })
+        if (!foundInstructor) return res.send("Instructor not found!")
 
-    const keys = Object.keys(req.body)
-
-    for (key of keys) {
-
-        if (req.body[key] == "") {
-            return res.send('Please, fill all fields')
+        const instructor = {
+            ...foundInstructor,
+            age: age(foundInstructor.birth),
+            services: foundInstructor.services.split(","),
+            created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at),
         }
+
+        return res.render("instructors/show", { instructor })
     }
 
-    let { avatar_url, birth, name, services, gender } = req.body
+    exports.post = function(req, res) {
 
-    birth = Date.parse(birth)
-    const created_at = Date.now()
-    const id = Number(data.instructors.length + 1)
+        const keys = Object.keys(req.body)
 
-    data.instructors.push({
-        id,
-        avatar_url,
-        name,
-        birth,
-        services,
-        gender,
-        created_at
-    })
+        for (key of keys) {
 
-    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
-        if (err) return res.send("Write file error!")
-        return res.redirect("/instructors")
-    })
-}
+            if (req.body[key] == "") {
+                return res.send('Please, fill all fields')
+            }
+        }
+
+        let { avatar_url, birth, name, services, gender } = req.body
+
+        birth = Date.parse(birth)
+        const created_at = Date.now()
+        const id = Number(data.instructors.length + 1)
+
+        data.instructors.push({
+            id,
+            avatar_url,
+            name,
+            birth,
+            services,
+            gender,
+            created_at
+        })
+
+        fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+            if (err) return res.send("Write file error!")
+            return res.redirect("/instructors")
+        })
+    }
